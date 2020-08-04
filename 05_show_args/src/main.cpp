f1dd6ba75246cd21f0a187306609ca3b3699f120
Pastor Roxana <roxana.pastor@yahoo.com>
2020-08-04 19:27:54 +0300

    Some updates

--- 05_show_args/src/main.cpp
+++ 05_show_args/src/main.cpp
@@ -6,21 +6,26 @@
 int main(int argc, char *argv[])
 {
 	
+	std::string argumentAsString = argv[1];
+	const char* argumentAsCharArray = argumentAsString.c_str();
+
 	if (argc <= 1)
 	{
 		printf("No program arguments found.\n");
 	}
-
-	
-	std::string argumentAsString = argv[1];
-	const char* argumentAsCharArray = argumentAsString.c_str();
-	//char[argumentAsString.length()] charArray;
-
-	for (int i = 0; i < argumentAsString.length(); i++) {
-	
-		printf(argv[i]);
-		printf("/n");
+	else {
+		printf("&argumentAsCharArray\n");
 	}
+
+	
+
+
+
+//	for (int i = 0; i < argumentAsString.length(); i++) {
+	
+		//printf("%s\n","This is a string");
+		//printf("\n");
+	//}
 
 
 
