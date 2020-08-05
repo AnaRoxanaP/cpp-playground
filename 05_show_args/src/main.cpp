2592d7491b6d49c403ba295e2719554b78ba4a24
Pastor Roxana <roxana.pastor@yahoo.com>
2020-08-05 19:21:33 +0300

    update for 05 app

--- 05_show_args/src/main.cpp
+++ 05_show_args/src/main.cpp
@@ -1,34 +1,12 @@
-#include <stdio.h>
-
 #include <iostream>
 #include <string>
 
 int main(int argc, char *argv[])
 {
-	
-	std::string argumentAsString = argv[1];
-	const char* argumentAsCharArray = argumentAsString.c_str();
+	for (int i = 0; i < argc; i++) {
 
-	if (argc <= 1)
-	{
-		printf("No program arguments found.\n");
+		std::cout << argv[i] << "\n" << std::endl;
 	}
-	else {
-		printf("&argumentAsCharArray\n");
-	}
-
-	
-
-
-
-//	for (int i = 0; i < argumentAsString.length(); i++) {
-	
-		//printf("%s\n","This is a string");
-		//printf("\n");
-	//}
-
-
-
 
 	return 0;
 }
