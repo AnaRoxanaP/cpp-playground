ddd03c9f46910921a957147433868a2ac060b731
Pastor Roxana <roxana.pastor@yahoo.com>
2020-08-14 22:48:46 +0300

    improvement

--- 15_game_of_life/src/main.cpp
+++ 15_game_of_life/src/main.cpp
@@ -8,10 +8,13 @@
 
 const int MIN_DIMENTION = 10;
 
-std::string Block[MIN_DIMENTION] = { "          " ,"          " ,"          " ,"          ", "    **    ", "    **    " ,"          " ,"          " ,"          " ,"          "};
-std::string Boat[MIN_DIMENTION] = { "          " ,"          " ,"          " ,"   **     ", "   * *    ", "    *     " ,"          " ,"          " ,"          " ,"          " };
-std::string Blinker[MIN_DIMENTION]= { "          " ,"          " ,"          " ,"          ", "   ***    ", "          " ,"          " ,"          " ,"          " ,"          " };
-std::string Beacon[MIN_DIMENTION]= { "          " ,"          " ,"          " ,"   **     ", "   *      ", "      *   " ,"     **   " ,"          " ,"          " ,"          " };
+std::string Block[MIN_DIMENTION] = { ".........." ,".........." ,".........." ,"..........", "....**....", "....**...." ,".........." ,".........." ,".........." ,".........." };
+std::string Boat[MIN_DIMENTION] = { ".........." ,".........." ,".........." ,"...**.....", "...*.*....", "....*....." ,".........." ,".........." ,".........." ,".........." };
+std::string Blinker[MIN_DIMENTION]= {  ".........." , ".........." , ".........." , "..........", "...***....",  ".........." , ".........." , ".........." , ".........." , ".........." };
+std::string Beacon[MIN_DIMENTION]= {  "....**...." , "....**.*.." , ".........." ,"...**.*...", "...*..*...", "....*.*..." ,".**..**..." , "...**..*.." , "....***...", "....***..." };
+//std::string Beacon[MIN_DIMENTION]= {  ".........." , ".........." , ".........." ,"...**.....", "...*......", "......*..." ,".....**..." , ".........." , "..........", ".........." };
+
+
 class Matrix
 {
 private:
@@ -50,50 +53,50 @@ public:
 
 	void setCellXY(size_t x, size_t y, char cell_content)
 	{
-		// TODO: add functionality
 		grid_matrix[y][x] = cell_content;
 	}
 
 
-	//Here I am verifing if a cell will be alive or dead, but I have some issues
-	void liveOrDie(std::string mat[MIN_DIMENTION][MIN_DIMENTION]) {
-		std::string mat2[MIN_DIMENTION][MIN_DIMENTION];
+	//Here I am verifing if a cell will be alive or dead
+	void liveOrDie(Matrix mat) {
+		Matrix mat2 = mat;
 		for (int i = 0; i < MIN_DIMENTION; i++) {
 			for (int j = 0; j < MIN_DIMENTION; j++) {
-				mat2[i][j] = mat[i][j];                   //mat2 is a copy of my initial matrix
+				mat2.grid_matrix[i][j] = mat.grid_matrix[i][j];         //mat2 is a copy of my initial matrix
 			}
 		}
 
-		for (int k = 1; k < MIN_DIMENTION-1; k++) {
-			for (int l = 1; l < MIN_DIMENTION-1; l++) {
+		for (int k = 1; k < MIN_DIMENTION-1 ; k++) {
+			for (int l = 1; l < MIN_DIMENTION-1 ; l++) {
 				int life = 0;
-				for (int m = 0; m < 2; m++) {
-					for (int n = 0; n < 2; n++) {
-						if (m != 1 && n != 1) {
-							if (mat2[m][n] == mat2[k][l])
-								life++;
+				for (int m = -1; m < 2; m++) {
+					for (int n = -1; n < 2; n++) {
+						if (m != 0 && n != 0) {
+							if (mat2.grid_matrix[k+m][l+n] == "*")
+								++life;
 						}
 					}
 				}
 				if (life < 2) {
-					setCellXY(k, l, ' ');
+					mat.setCellXY(k, l, '.');
 				}
 				else if (life == 2 || life == 3) {
-						setCellXY(k, l, '*');
+					mat.setCellXY(k, l, '*');
 				}
-				else if(life > 3) {
-					setCellXY(k, l, ' ');
+				else if (life > 3) {
+					mat.setCellXY(k, l, '.');
 				}
 			}
 		}
 	}
+	
 
 	void print()
 	{
 		for (int i = 0; i < line; i++) {
 			for (int j = 0; j < column; j++) {
 
-				std::cout << grid_matrix[i][j];
+				std::cout << grid_matrix[i][j]<<" ";
 			}
 			std::cout << std::endl;
 		}
@@ -104,18 +107,20 @@ public:
 
 
 
+
+
 int main()
 {
 	Matrix mat(20,10);
-	mat.setMatrix(mat, Block);
-	mat.print();
-	//mat.liveOrDie;
-	mat.setMatrix(mat, Boat);
-	mat.print();
 	mat.setMatrix(mat, Beacon);
-	mat.print();
-
 	
 
-	//return 0;
+	while (true) {
+		mat.print();
+		mat.liveOrDie(mat);		
+		system("CLS");
+	} 
+	
 }
+
+
